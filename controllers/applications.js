b162ce1f1193dd5dc1e6de58d447ef38d319bfab
melabreu <melissa.abreu84@gmail.com>
2025-09-04 15:17:09 -0400

    included the application controller

--- controllers/applications.js
+++ controllers/applications.js
@@ -16,4 +16,17 @@ router.get('/new', async (req, res) => {
   res.render('applications/new.ejs');
 });
 
+router.post('/', async (req, res) => {
+  try {                            // Look up the user from req.session
+    const currentUser = await User.findById(req.session.user._id);
+    // Push req.body (the new form data object) to the applications array of the current user
+    currentUser.applications.push(req.body);
+    await currentUser.save();               // Save changes to the user
+    res.redirect(`/users/${currentUser._id}/applications`);   // Redirect back to the applications index view
+  } catch (error) {           // If any errors, log them and redirect back home
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 module.exports = router;
