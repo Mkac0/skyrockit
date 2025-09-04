450c6d949ba7d95e4cef537080814418a9096312
melabreu <melissa.abreu84@gmail.com>
2025-09-04 12:49:58 -0400

    cleaned up html and server

--- models/user.js
+++ models/user.js
@@ -1,6 +1,28 @@
 const mongoose = require('mongoose');
 
-const userSchema = mongoose.Schema({
+//    ------     Schema    ------
+const applicationSchema = new mongoose.Schema({
+  company: {
+    type: String,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  notes: {
+    type: String,
+  },
+  postingLink: {
+    type: String,
+  },
+  status: {
+    type: String,
+    enum: ['interested', 'applied', 'interviewing', 'rejected', 'accepted'],  // enum: enumerate(validator)
+  },
+});
+
+const userSchema = new mongoose.Schema({
   username: {
     type: String,
     required: true,
@@ -9,6 +31,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  applications: [applicationSchema],
 });
 
 const User = mongoose.model('User', userSchema);
