1636b837f033ecbc688c104055ec2e2fab2d0583
melabreu <melissa.abreu84@gmail.com>
2025-09-04 16:25:51 -0400

    setup the edit route

--- controllers/applications.js
+++ controllers/applications.js
@@ -32,6 +32,19 @@ router.get('/:applicationId', async (req, res) => {
   }
 });
 
+router.get('/:applicationId/edit', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.session.user._id);
+    const application = currentUser.applications.id(req.params.applicationId);
+    res.render('applications/edit.ejs', {
+      application: application,
+    });
+  } catch (error) {
+    console.log(error);
+    res.redirect('/');
+  }
+});
+
 router.post('/', async (req, res) => {
   try {                            // Look up the user from req.session
     const currentUser = await User.findById(req.session.user._id);
